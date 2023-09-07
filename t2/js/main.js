7fdd7805c74b1b299c67413da869e08e255511a7
Liz_I971 <ivanovaliza0053@gmail.com>
2023-09-07 15:14:37 +0300

    dfghj

---
+++ t2/js/main.js
@@ -0,0 +1,5 @@
+let = city = prompt('Название города:');
+let = year = prompt('Год образования:');
+year = Number(year);
+let = peoples = prompt('Население:');
+alert('Городу '+ city + ' исполнилось ' +(2023-year)+ ' лет с момента его образования. Население - '+peoples+' человек');

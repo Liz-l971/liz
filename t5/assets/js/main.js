ea3faeec21926cf278fa3111b1b3df64fe9493e5
Liz_I971 <ivanovaliza0053@gmail.com>
2023-09-08 12:40:31 +0300

    rgtyuji

--- t5/assets/js/main.js
+++ t5/assets/js/main.js
@@ -1,6 +1,6 @@
 let chislo = prompt('Введите цифру:');
 let block = '<div class ="block"></div>';
 let block_list = document.querySelector('.block_wrapper');
-for(let i = 0;i<chislo;i++){
+for(let i = 0;i<chislo-1;i++){
     block_list.insertAdjacentHTML('beforeend',block);
 }

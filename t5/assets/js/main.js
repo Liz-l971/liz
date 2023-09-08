e78e1d8bab2c7ee11030c5463e6d4b90dd8d6a44
Liz_I971 <ivanovaliza0053@gmail.com>
2023-09-08 12:04:28 +0300

    tdfgu

---
+++ t5/assets/js/main.js
@@ -0,0 +1,6 @@
+let chislo = prompt('Введите цифру:');
+let block = '<div class ="block"></div>';
+let block_list = document.querySelector('.block_wrapper');
+for(let i = 0;i<=chislo;i++){
+    block_list.insertAdjacentHTML('beforeend',block);
+}

ee1960700f47b72f9465388dd62ffbd5c24da882
Liz_I971 <ivanovaliza0053@gmail.com>
2023-10-05 16:41:59 +0300

    fsdghbj

--- js/t8/assets/js/script.js
+++ js/t8/assets/js/script.js
@@ -3,7 +3,6 @@ let content = document.getElementsByClassName("content");
 for(let i = 0;i<accordion.length;i++){
     accordion[i].onclick = function(){
         content[i].classList.toggle("show");
-        accordion[i].classList.toggle("click");
-        
+        accordion[i].classList.toggle("click");  
     }
 }
